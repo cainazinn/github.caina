4677bd269ca99cbbd08bb8541bc8bd7ea3369f67
Cainãzinn <cainacavalcante352@gmail.com>
2025-06-13 00:02:38 -0300

    Update script.js

--- Estudos em JS/Aulas/Aula 12 exercícios/ex014/script.js
+++ Estudos em JS/Aulas/Aula 12 exercícios/ex014/script.js
@@ -1,18 +1,18 @@
 function carregar(){
-    var div1 = document.querySelector(`div#msg`)
+    var div = document.querySelector(`div#msg`)
     var imagem = document.querySelector(`img#imagem`)
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 
-    div1.innerHTML = `Agora são ${hora} horas.`
+    var hora = data.getHours()
+    div.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
-        imagem.src = `manha.png` 
-        document.body.style.background = "#f9c545"
-    }else if(hora >= 12 && hora < 18){
-        imagem.src = `tarde.png`
-        document.body.style.background = "#faac50"
-    }else{
-        imagem.src = `noite.png`
-        document.body.style.background = "#3573ae"
+        imagem.src = `manha_circulo.png`
+        document.body.style.background = `#f9c545`
+    } else if(hora >= 12 && hora < 18){
+        imagem.src = `tarde_circulo.png`
+        document.body.style.background = `#faac50`
+    } else{
+        imagem.src = `noite_circulo.png`
+        document.body.style.background = `#3c3c3c`
     }
+    
 }
